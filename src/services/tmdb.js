d20dc37c4db83a91876603781f6558b9142be554
Furkan Altaca <furkanaltaca@gmail.com>
2025-05-22 11:17:10 +0300

    feat(movie-service): add filter for get movie function

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -1,18 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_KEY = 'ff53509c0b9d5ff2abbb84cd9bad6ace';
-const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = "ff53509c0b9d5ff2abbb84cd9bad6ace";
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const fetchRandomMovie = async () => {
   try {
+    const langs = ["tr", "en"];
+    const selectedLang = langs[Math.floor(Math.random() * langs.length)];
+
     const randomPage = Math.floor(Math.random() * 500) + 1;
-    
+
     const response = await axios.get(`${BASE_URL}/discover/movie`, {
       params: {
         api_key: API_KEY,
-        language: 'tr-TR',
-        sort_by: 'popularity.desc',
+        language: "tr-TR",
+        sort_by: "popularity.desc",
         page: randomPage,
+        with_original_language: selectedLang,
       },
     });
 
@@ -22,10 +26,10 @@ export const fetchRandomMovie = async () => {
       const randomIndex = Math.floor(Math.random() * movies.length);
       return movies[randomIndex];
     } else {
-      throw new Error('No movies found');
+      throw new Error("No movies found");
     }
   } catch (error) {
-    console.error('Error fetching movie:', error.message);
+    console.error("Error fetching movie:", error.message);
     throw error;
   }
 };
